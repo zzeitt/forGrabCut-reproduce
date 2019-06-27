2282336293aa3401fd5ba67946c3c3065ab0ebb9
zeit <zei.t@qq.com>
2019-06-27 12:08:22 +0800

    debuged for kmeans clustering

--- 20190619_GRABCUT/main.cpp
+++ 20190619_GRABCUT/main.cpp
@@ -1,13 +1,21 @@
 #include "grabcut_client.h"
 
 int main() {
-  //GrabCutClient gc_client(
+  // GrabCutClient gc_client(
   //    "D:\\NiseEngFolder\\MyDocument\\MyPictures\\forMiscellaneous\\20190619_"
   //    "CV_grabcut\\owl.jpg");
-  Mat img_temp(1, 2, CV_8UC3);
+  Mat img_temp(2, 3, CV_8UC3);
   img_temp.at<Vec3b>(0, 0) = Vec3b(0, 255, 0);
   img_temp.at<Vec3b>(0, 1) = Vec3b(0, 0, 255);
+  img_temp.at<Vec3b>(0, 2) = Vec3b(0, 255, 1);
+  img_temp.at<Vec3b>(1, 0) = Vec3b(0, 127, 0);
+  img_temp.at<Vec3b>(1, 1) = Vec3b(0, 0, 127);
+  img_temp.at<Vec3b>(1, 2) = Vec3b(0, 127, 1);
   Mat mask_alpha(img_temp.size(), CV_8UC1, Scalar(GC_FGD));
+  mask_alpha.at<uchar>(1, 0) = GC_BGD;
+  mask_alpha.at<uchar>(1, 1) = GC_BGD;
+  mask_alpha.at<uchar>(1, 2) = GC_BGD;
+
   GrabCutMethod gc_method(2, img_temp.dims, 2, img_temp.dims);
   gc_method.initPixelsVec(img_temp, mask_alpha);
   gc_method.clusterPixels();
